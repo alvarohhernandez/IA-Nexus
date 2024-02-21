f6facc422bc9fb907cc7fa577351b9f0b1e451aa
carlos-crea <carlos.ed0205@ciencias.unam.mx>
2024-02-20 21:59:51 -0600

    Update main.py
    avance 1

--- Practica_02/main.py
+++ Practica_02/main.py
@@ -1,4 +1,7 @@
 # Representación del laberinto
+import queue
+
+
 laberinto = [
     ["E", 0, 1, 0],
     [1, 0, 1, 0],
@@ -7,36 +10,56 @@ laberinto = [
 ]
 
 class Agente:
+    #Constructor del agente en una posicion de la matriz
     def __init__(self, posicion):
         self.posicion = posicion  # La posición inicial del agente
 
+    # Función que permite mover al agente dentro de la matriz
     def mover(self, direccion, laberinto):
         x, y = self.posicion
-        if direccion == "arriba" and x > 0:
+        if direccion == "arriba" and x > 0 and laberinto[x-1][y] == 0 :
+            laberinto[x][y] = 2
             self.posicion = [x-1, y]
-        elif direccion == "abajo" and x < len(laberinto) - 1:
+        elif direccion == "abajo" and x < len(laberinto) - 1 and laberinto[x+1][y] == 0 :
             self.posicion = [x+1, y]
-        elif direccion == "izquierda" and y > 0:
+            laberinto[x][y] = 2
+        elif direccion == "izquierda" and y > 0 and laberinto[x][y-1] == 0 :
             self.posicion = [x, y-1]
-        elif direccion == "derecha" and y < len(laberinto[0]) - 1:
+            laberinto[x][y] = 2
+        elif direccion == "derecha" and y < len(laberinto[0]) - 1 and laberinto[x][y+1] == 0 :
             self.posicion = [x, y+1]
+            laberinto[x][y] = 2
         else:
-            print("El movimiento no es válido D:")
+            return 0
 
-agente = Agente([0,0])
-
-def encontrar_salida(agente, laberinto):
-    movimientos = ["derecha", "abajo", "abajo", "derecha", "abajo", "derecha"]
+def encontrar_salida(agente, laberinto, ruta_seguida):
+    movimientos = ["arriba", "abajo", "izquierda", "derecha"]
+    ruta_seguida.put(agente.posicion.copy()) # Almacena la posición actual en la ruta
+    print("Ruta seguida por el agente:", list(ruta_seguida.queue))
+    contador= 0
 
     for movimiento in movimientos:
-        agente.mover(movimiento, laberinto)
+        if agente.mover(movimiento, laberinto) != 0:
+            agente.mover(movimiento, laberinto)
+            encontrar_salida(agente, laberinto, ruta_seguida)
 
-        print(f"Movimiento: {movimiento}, Posición: {agente.posicion}")
+        if agente.mover(movimiento, laberinto) == 0:
+            contador+= 1
+            if contador == 4 :
+                ultimo = ruta_seguida.get
+                back = Agente(ultimo)
+                encontrar_salida(back, laberinto, ruta_seguida)
 
         if laberinto[agente.posicion[0]][agente.posicion[1]] == "S":
             print("Encontré la salida :D")
+            print("Ruta seguida por el agente:", ruta_seguida)
             return
 
     print("iiiiii no encontré la salida")
 
-encontrar_salida(agente, laberinto)
+# Crear una instancia de la clase Agente
+agente = Agente([0, 0])
+ruta = queue.LifoQueue(100)
+
+# Llamar a la función para encontrar la salida
+encontrar_salida(agente, laberinto, ruta)
