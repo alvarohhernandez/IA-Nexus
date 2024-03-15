e601cc6bc44e9c84e1871799b4fd2dc37541cdc8
carlos-crea <carlos.ed0205@ciencias.unam.mx>
2024-03-15 01:42:31 -0600

    Comentarios

--- Practica_04/main.py
+++ Practica_04/main.py
@@ -5,20 +5,24 @@ class Agente:
         self.inicio = inicio
         self.fin = fin
 
+# Busca la entrada de el laberinto 
 def encontrar_entrada(laberinto):
     for fila in range(len(laberinto)):
         for columna in range(len(laberinto[0])):
-            if laberinto[fila][columna] == "E":
+            if laberinto[fila][columna] == "E": #La cadena 'E' determina la entrada
                 return (fila, columna)
     return None
 
+# Busca la salida de el laberinto 
 def encontrar_salida(laberinto):
     for fila in range(len(laberinto)):
         for columna in range(len(laberinto[0])):
-            if laberinto[fila][columna] == "S":
+            if laberinto[fila][columna] == "S": #La cadena 'S' determina la entrada
                 return (fila, columna)
     return None
 
+# Transforma los strings o caracteres '1' y '0'
+# de el laberinto en ints
 def parse_laberinto(laberinto):
     for fila in range(len(laberinto)):
         for columna in range(len(laberinto[0])):
@@ -28,18 +32,25 @@ def parse_laberinto(laberinto):
                 laberinto[fila][columna] = 1
     return laberinto
 
+
+# Calcula el costo estimado entre un punto y otro 
 def distancia_manhattan(pos1, pos2):
     return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
+# Regresa el costo de la ruta hasta ese momento
 def get_costo_g(ruta, actual):
     return ruta.get(actual, (0,0))[0] + 1
 
+# Recresa el costo estimado ente un vecino y la salida.
 def get_costo_h(vecino, fin):
     return distancia_manhattan(vecino, fin)
 
+# Calcula la funcion de costo
 def get_costo_f(g,h):
     return g + h
 
+# Regresa una lista con los vecinos de la posicion actual.
+# Vecino= Casilla distinta de 1 y que no ha sido visitada.
 def encuentra_vecinos(pos, laberinto, visitados):
     vecinos = []
     x, y = pos
@@ -65,11 +76,12 @@ def encuentra_salida(agente, laberinto):
     while cola_prioridad:
         _, actual = heapq.heappop(cola_prioridad)
 
-        if actual == fin:
+        if actual == fin: #terminamos el loop si llegamos a la salida
             break
 
         vecinos = encuentra_vecinos(actual, laberinto, visitados)
 
+        # Calculamos el costo de todos los vecinos
         for vecino in vecinos:
             costo_g = get_costo_g(ruta, actual)
             costo_h = get_costo_h(vecino, fin)
@@ -82,10 +94,11 @@ def encuentra_salida(agente, laberinto):
     if actual != fin:
         return {}, -1
     else:
-        ruta_final = reconstruir_ruta(ruta, inicio, fin)
-        costo_final = ruta[fin][0]
+        ruta_final = reconstruir_ruta(ruta, inicio, fin) # ruta seguida
+        costo_final = ruta[fin][0] # costo de la ruta seguida
         return ruta_final, costo_final
 
+# Reconstruye la ruta seguida para poder mostrala en terminal.
 def reconstruir_ruta(ruta, inicio, fin):
     ruta_final = []
     actual = fin
@@ -101,6 +114,7 @@ def reconstruir_ruta(ruta, inicio, fin):
     
     return ruta_final
 
+#laberinto de prueba
 laberinto = [
     [0, 0, 0, 1, "S"],
     [0, 1, 0, 0, 0],
